c24131e85bb57ac865759df450ddd1297e73bb1e
Cagri Kirtis <cagrikirtis17@gmail.com>
2024-03-06 22:28:39 +0300

    First Coommit

---
+++ SQL12.sql
@@ -0,0 +1,26 @@
+--SELECT length FROM film
+--WHERE length > 
+--(
+--SELECT AVG(length) FROM film
+--);
+
+
+--SELECT COUNT(*) FROM film
+--WHERE rental_rate = 
+--(
+--SELECT MAX(rental_rate) FROM film
+--);
+
+
+--SELECT * FROM film 
+--WHERE (rental_rate = (SELECT MIN(rental_rate) FROM film) AND replacement_cost = (SELECT MIN(replacement_cost) FROM film));
+
+--SELECT * FROM payment
+--WHERE amount = 
+--(
+--SELECT MAX(amount) FROM payment
+--);
+
+
+
+
